ed37a776ad6e886ab99a6b6bebef825bf5beb0f1
Russell <codewithsurrell@gmail.com>
2026-07-03 18:05:41 +0200

    Remove old Vite source project

--- games/money-blocks/source/src/hooks/useSettings.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { SettingsContext } from '../context/SettingsContext';
-
-export function useSettings() {
-  const ctx = useContext(SettingsContext);
-  if (!ctx) throw new Error('useSettings must be used within a SettingsProvider');
-  return ctx;
-}
-
--- games/money-blocks/source/src/hooks/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { ThemeContext } from '../context/ThemeContext';
-
-export function useTheme() {
-  const ctx = useContext(ThemeContext);
-  if (!ctx) throw new Error('useTheme must be used within a ThemeProvider');
-  return ctx;
-}
-
